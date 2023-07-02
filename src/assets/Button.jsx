6ffff94b7eb1103e9104083c909074b1aecc20f8
farhan688 <farhanaditya688@gmail.com>
2023-07-02 15:29:42 +0700

    first commit

--- src/assets/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ icons, onClick }) {
-  return (
-    <a href="#" onClick={onClick} className="button">
-      {icons}
-    </a>
-  );
-}
